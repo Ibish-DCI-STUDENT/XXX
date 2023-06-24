e98f26f5fc57e79c48c541412d5719675293bd44
Ibish <ibish.s@live.com>
2023-06-24 18:32:43 +0200

    Edded the email address

--- newuser.sh
+++ newuser.sh
@@ -6,6 +6,13 @@ read -p "Enter the new username: " username
 # Create a new user
 sudo useradd -m $username
 
+# Prompt for password
+read -s -p "Enter password: " password
+echo
+
+# Set the password for the user
+echo "$username:$password" | sudo chpasswd
+
 # Prompt for the new group name
 read -p "Enter the new group name: " groupname
 
@@ -19,8 +26,10 @@ echo "New user '$username' and group '$groupname' created successfully."
 
 
 # Set  variables
-export MY_VARIABLE=" IS everything OK"
+export MY_VARIABLE=" IS everything OK ?"
 export ANOTHER_VARIABLE="If So Send me an Email"
+
+echo "Ibish.s@live.com"
 
 # Use the variables
 echo $MY_VARIABLE
